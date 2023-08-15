8509e9945dbcf902c69f94429690bb63e3ff0d0b
M.one <mouadelhourre50@gmail.com>
2023-08-15 15:56:13 +0300

    display all alphabets 10x times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet:print alphabets 10X more a-z
+ * Return: 0
+ */
+void print_alphabet_x10(void) {
+    
+char j,i;
+
+for(j =0; j<= 10; j++){
+
+	for (i = 97; i <= 122; i++)
+	{
+           _putchar(i);
+	}
+	_putchar('\n');
+}
+
+ 
+}
