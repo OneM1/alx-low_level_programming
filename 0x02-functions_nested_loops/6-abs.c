fb94cf0603e08e44659e18658bbf7c5b594c7d39
M.one <mouadelhourre50@gmail.com>
2023-08-16 06:47:02 +0300

    display the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main: function that computes the absolute value of an integer
+ * @i: is the interger need to check
+ * return the absolute value of int n
+ */
+int _abs(int n)
+	{
+	
+	if(n>=0)
+	{
+	return(n);
+	}
+	
+	return(-n);	
+	}
+
