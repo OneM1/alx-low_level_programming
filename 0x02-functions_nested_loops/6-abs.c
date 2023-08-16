77c898f2adc7c179c1d91aeb7129e7883efec6ae
M.one <mouadelhourre50@gmail.com>
2023-08-16 12:54:57 +0300

    print alphabet

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,10 +4,8 @@
 /**
  * abs - function that computes the absolute value of an integer
  *
- * @i:  is the int that will use for the argument of the function
- * 
+ * @i:  is the int that will use for the argument of the function 
  * Return: 0
- * 
 */
 int _abs(int i)
 {
