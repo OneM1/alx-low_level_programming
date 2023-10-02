ddf0f890545d825b8baf54fa3d94e1e2f229af47
OneM1 <mouadelhourre50@gmail.com>
2023-10-02 15:34:03 +0300

    master

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
+ * print_binary - prints the binary representation of a number.
  * @n: number to convert in binary
  */
 void print_binary(unsigned long int n)
