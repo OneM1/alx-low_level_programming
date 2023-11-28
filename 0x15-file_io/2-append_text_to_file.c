ea97de5391be5a68ddd4c2ac2307d267656cc91d
OneM1 <mouadelhourre50@gmail.com>
2023-11-28 05:57:03 +0300

    master

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * append_text_to_file - a function that appends text at the end of a file
+ * Description: function that appends to text at the end of a file
+ * Return: 1 on success or -1 for failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	
+}
