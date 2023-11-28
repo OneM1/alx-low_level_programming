1476067b07c9a757e216e6501dec6334505039b6
OneM1 <mouadelhourre50@gmail.com>
2023-11-28 05:53:22 +0300

    master

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,30 +5,29 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: The name of the file to be created.
- * @text_content: The text to write into the file.
- *
+ * create_file - Creates a file and writes text to it.
+ * @filename: Name of the file to create.
+ * @text_content: Text to write into the file.
  * Description: This function creates a file with the specified name and writes
- * the provided text into it. If the file already exists, it truncates it. The
- * function returns -1 on failure, 1 on success.
- * Return: 1 on success, -1 on failure.
- */
+ * the provided text into it
+ * returns -1 on failure and 1 on success.
+ **/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t n_written;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL) {
+	if (text_content)
+	{
 		n_written = write(fd, text_content, strlen(text_content));
-		if (n_written == -1) 
+		if (n_written == -1)
 		{
 			close(fd);
 			return (-1);
@@ -38,4 +37,12 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+
+
+
+
+
+
+
+
 
